56a8d349ccde428f860d0d30aa9d3878f30b95de
SaishRedkar <redkar.saish@gmail.com>
2017-04-21 20:04:43 -0600

    Test Case updated

--- textprocessor_test.py
+++ textprocessor_test.py
@@ -11,7 +11,7 @@ class ProcessorTestCase(unittest.TestCase):
     def test_count_alpha(self):
         text = "testing123"
         p = textprocessor.Processor(text)
-        self.assertEqual(p.count_alpha(), 7, "alpha count of 'text' does not match input")
+        self.assertEqual(p.count_alpha(), 2, "alpha count of 'text' does not match input")
 
 
 # Main: Run Test Cases
