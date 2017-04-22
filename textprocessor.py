e9c57655c84396060ac3cd2ed43969ba752b1c93
SaishRedkar <redkar.saish@gmail.com>
2017-04-21 20:07:58 -0600

    Test Case updated to make it pass

--- textprocessor.py
+++ textprocessor.py
@@ -22,21 +22,3 @@ class Processor:
 
         alpha = re.compile(r'[a-zA-Z]')
         return len(alpha.findall(self.text))
-
-    # def count_numeric(self):
-    #     """
-    #     Count number of numeric characters in text
-    #     :return: Number of numeric characters
-    #     """
-
-    #     alpha = re.compile(r'[0-9]')
-    #     return len(alpha.findall(self.text))
-
-    # def count_vowels(self):
-    #     """
-    #     Count number of vowels in text
-    #     :return: Number of vowels
-    #     """
-        
-    #     vowels = re.compile(r'[aeiou]', re.IGNORECASE)
-    #     return len(vowels.findall(self.text))
