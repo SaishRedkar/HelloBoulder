2bc418e861c8191267aa1c0faf1177e2c33a505a
SaishRedkar <redkar.saish@gmail.com>
2017-04-21 19:52:41 -0600

    Changed test case value

--- textprocessor_test.py
+++ textprocessor_test.py
@@ -11,17 +11,17 @@ class ProcessorTestCase(unittest.TestCase):
     def test_count_alpha(self):
         text = "testing123"
         p = textprocessor.Processor(text)
-        self.assertEqual(p.count_alpha(),7, "alpha count of 'text' does not match input")
+        self.assertEqual(p.count_alpha(), 7, "alpha count of 'text' does not match input")
 
     def test_count_numeric(self):
         text = "testing123"
         p = textprocessor.Processor(text)
-        self.assertEqual(p.count_numeric(),3, "numeric count of 'text' does not match input") 
+        self.assertEqual(p.count_numeric(), 4, "numeric count of 'text' does not match input") 
 
     def test_count_vowels(self):
         text = "testing123"
         p = textprocessor.Processor(text)
-        self.assertEqual(p.count_vowels(),2, " vowel count of'text' does not match input")  
+        self.assertEqual(p.count_vowels(), 2, " vowel count of'text' does not match input")  
 
 
 # Main: Run Test Cases
